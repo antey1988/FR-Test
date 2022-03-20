0d5698bdcbc074fd43c6009f06d1f5a9bf11d3f1
Oleg Petrov <Antey1988@gmail.com>
2022-03-20 12:04:25 +0300

    Added dependencies

---
+++ src/test/java/com/antey/fr/FrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antey.fr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
